826ba47a60500997e09379d77e285942021f61e4
druidmaciek <maciej@janowski.dev>
2021-05-08 20:14:01 +0200

    change version

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,10 @@
 <script>
 	import Header from '../components/Header.svelte';
 	import Layout from '../components/Layout.svelte';
-  import Testimonials from "../components/Testimonials.svelte";
-  import Footer from "../components/Footer.svelte";
-  import Contact from "../components/Contact.svelte";
-  import Services from "../components/Services.svelte";
+	import Testimonials from '../components/Testimonials.svelte';
+	import Footer from '../components/Footer.svelte';
+	import Contact from '../components/Contact.svelte';
+	import Services from '../components/Services.svelte';
 </script>
 
 <svelte:head>
@@ -14,19 +14,17 @@
 <Header />
 
 <Layout>
-	<Services/>
+	<Services />
 </Layout>
 
 <div id="testimonials" class="bg-gray-100 py-12">
 	<Layout>
-		<Testimonials/>
+		<Testimonials />
 	</Layout>
 </div>
 
 <Layout>
-	<Contact/>
+	<Contact />
 </Layout>
 
-
-
-<Footer/>
+<Footer />
